cd645704f047ed3ee6cae222050d6b5fc66a597a
Md.Alimur Razi <alimurrazi570@gmail.com>
2018-11-19 12:59:24 +0600

    local storage data add

--- 1st_task/app.js
+++ 1st_task/app.js
@@ -5,7 +5,7 @@ var closeButton = document.querySelector(".close-button");
 function toggleModal() {
     modal.classList.toggle("show-modal");
 }
-
+ 
 function windowOnClick(event) {
     if (event.target === modal) {
         toggleModal();
@@ -14,9 +14,36 @@ function windowOnClick(event) {
 
 function submitData()
 {
+	toggleModal();
 	console.log("why buddy");
-	console.log(localStorage);
+  //  debugger;
+
+    var allContact=JSON.parse(localStorage.getItem("allContactLS"));
+    console.log(allContact);
+ 
+    if(allContact==null)
+    	allContact=[];
+
+    var firstName=document.getElementById("firstName").value;
+    var lastName=document.getElementById("lastName").value;
+    var phoneNumber=document.getElementById("phoneNumber").value;
+    var contact={
+    	"firstName": firstName,
+    	"lastName": lastName,
+    	"phoneNumber": phoneNumber
+    };
+
+    allContact.push(contact);
+    localStorage.setItem("allContactLS",JSON.stringify(allContact));
+    showData();
 }
+
+
+function showData()
+{
+	
+}
+
 
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);

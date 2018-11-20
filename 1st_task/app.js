f80eba96f1229d74ae2e60bf5f047cbfe9d50ee6
Md.Alimur Razi <alimurrazi570@gmail.com>
2018-11-20 16:04:52 +0600

    table add

--- 1st_task/app.js
+++ 1st_task/app.js
@@ -1,6 +1,7 @@
 var modal = document.querySelector(".modal");
 var trigger = document.querySelector(".trigger");
 var closeButton = document.querySelector(".close-button");
+showData();
 
 function toggleModal() {
     modal.classList.toggle("show-modal");
@@ -19,7 +20,7 @@ function submitData()
   //  debugger;
 
     var allContact=JSON.parse(localStorage.getItem("allContactLS"));
-    console.log(allContact);
+   // console.log(allContact);
  
     if(allContact==null)
     	allContact=[];
@@ -35,15 +36,37 @@ function submitData()
 
     allContact.push(contact);
     localStorage.setItem("allContactLS",JSON.stringify(allContact));
-    showData();
+    showData(allContact);
 }
-
 
 function showData()
 {
-	
+     var contacts=JSON.parse(localStorage.getItem("allContactLS"));
+     var mainContent=document.getElementById("main-content");
+     mainContent.innerHTML="";
+     
+     mainContent.innerHTML="<table id='table'><tr><th>Name</th><th>Phone Number</th></tr>"
+
+     for (var i =0; i < contacts.length; i++) {
+      var table=document.getElementById("table");
+      var row=table.insertRow(i+1);
+      var coloumn1=row.insertCell(0);
+      var coloumn2=row.insertCell(1);
+      var coloumn3=row.insertCell(2);
+      var coloumn4=row.insertCell(3);
+      coloumn1.innerHTML=contacts[i].firstName+" "+contacts[i].lastName;
+      coloumn2.innerHTML=contacts[i].phoneNumber;
+      coloumn3.innerHTML="<button class='btn' onClick='updateData()' id="+i+">Update</button>";
+      coloumn4.innerHTML="<button>Delete</button>";
+     }
+     mainContent.insertAdjacentHTML('beforeend', '</table>');
+
 }
 
+function updateData()
+{
+   console.log(event.srcElement.parentNode);
+}
 
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);

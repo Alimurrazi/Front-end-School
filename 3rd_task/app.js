7f91e17de09d037ef71764c972c0bee150f18aff
Rana <alimur.razi@selise.ch>
2018-12-13 00:19:52 +0600

    hudai time lose

--- 3rd_task/app.js
+++ 3rd_task/app.js
@@ -1,332 +1,326 @@
- "use strict"
+"use strict"
 
-   var body = document.getElementsByTagName('body')[0];
-   var modal ;
-   var container;
-   var currentIndex;
-   var isEdit = false;
+var body = document.getElementsByTagName('body')[0];
+var modal;
+var container;
+var currentIndex;
+var currentCategory = "all";
+var isEdit = false;
 
- function init()
- {
+function init() {
     containerView();
     modalView();
- }
+}
 
- function containerView()
- {
+function containerView() {
     var containerContent = '<div class="container" id="container"></div>';
     body.innerHTML += containerContent;
 
     container = document.getElementById('container');
     titleIconView();
- }
+}
 
- function titleIconView(){
-     var titleIconContent = '<div class="grid title-icon">'+
-            '<i class="fa fa-check" aria-hidden="true"></i>'+
-         '</div>';    
-     container.innerHTML += titleIconContent;
-      titleView();
- }
+function titleIconView() {
+    var titleIconContent = '<div class="grid title-icon">' +
+        '<i class="fa fa-check" aria-hidden="true"></i>' +
+        '</div>';
+    container.innerHTML += titleIconContent;
+    titleView();
+}
 
- function titleView()
- {
-    var titleContent = '<div class="grid title">'+
-            '<h2>To do</h2>'+
-         '</div>';
+function titleView() {
+    var titleContent = '<div class="grid title">' +
+        '<h2>To do</h2>' +
+        '</div>';
     container.innerHTML += titleContent;
     typeTaskView();
- }
+}
 
- function typeTaskView()
- {
-    var typeTaskContent = '<div class="grid type-task">'+
-            '<input type="text" name="" placeholder="type task" class="type-new-task id="typeNewTask">'+
-         '</div>';
+function typeTaskView() {
+    var typeTaskContent = '<div class="grid type-task">' +
+        '<input type="text" name="" placeholder="type task" class="type-new-task id="typeNewTask">' +
+        '</div>';
     container.innerHTML += typeTaskContent;
-    addTaskView();     
- }
+    addTaskView();
+}
 
- function addTask()
- {
-    console.log(isEdit);
-    if (isEdit == true)
-    {
-      updateTask();
+function addTask() {
+    var value = document.getElementsByTagName('input')[0].value;
+
+    if (value != "") {
+        if (isEdit == true) {
+            updateTask();
+        } else {
+            console.log(value);
+            var allTask = JSON.parse(localStorage.getItem("allTask"));
+
+            if (allTask == null)
+                allTask = [];
+
+            var newTask = {
+                "title": value,
+                "status": 0
+            };
+
+            allTask.push(newTask);
+            localStorage.setItem("allTask", JSON.stringify(allTask));
+            showData();
+        }
     }
-    
     else
     {
-    var value = document.getElementsByTagName('input')[0].value;
-    if(value!="")
-    {
-     document.getElementsByTagName('input')[0].value = ""; 
-    console.log(value);
+       alert("Please Write Some Text!");
+    }
+}
+
+function updateTask() {
     var allTask = JSON.parse(localStorage.getItem("allTask"));
- 
-    if(allTask==null)
-        allTask = [];
+    allTask[currentIndex].title = document.getElementsByTagName('input')[0].value;
+    localStorage.setItem("allTask", JSON.stringify(allTask));
+    isEdit = false;
+    document.getElementsByTagName('input')[0].value = "";
+    var addBtn = document.getElementById('add-btn');
+    addBtn.innerHTML = "Add New Task";
 
-    var newTask={
-        "title": value,
-        "status": 0
-    };
-
-    allTask.push(newTask);
-    localStorage.setItem("allTask",JSON.stringify(allTask));
     showData();
-    }
-    }
-
- }
-
-function updateTask()
-{
-        debugger;
-        console.log(currentIndex);
-        var allTask = JSON.parse(localStorage.getItem("allTask"));
-        allTask[currentIndex].title = document.getElementsByTagName('input')[0].value;
-        localStorage.setItem("allTask",JSON.stringify(allTask));
-        isEdit = false;
-        document.getElementsByTagName('input')[0].value = "";
-        var addBtnLabel = document.getElementById('addBtnLabel');
-        addBtnLabel.innerHTML = "Add New Task";
-
-        showData();
 }
 
- function showData()
- {
-     var allTask = JSON.parse(localStorage.getItem("allTask"));
-     var taskListContent = document.getElementById("taskList");
-     taskListContent.innerHTML = "";
-     var count=0;
-     console.log(taskListContent);
-
-     if(allTask!=null)
-     {
-     for (var i = 0; i < allTask.length; i++) {
-        var specificTaskContent;
-        if(allTask[i].status == 1)
-        {
-            count++;
-            specificTaskContent = '<li class="specific-task" id="'+i+'">'+
-                  '<span class="category-icon" onclick="changeState("'+i+')">'+
-                  '<i class="fa fa-circle-o" aria-hidden="true"></i></span>'+
-                  '<span class="category-label">'+allTask[i].title+'</span>'+
-                  '<span class="category-modal-icon trigger" onclick="changeState("'+i+')">'+
-                  '<i class="fa fa-ellipsis-v">'+
-                  '</i><i class="fa fa-ellipsis-v"></i></span></li>';
-
-            taskListContent.innerHTML += specificTaskContent;
-
-            if(count == allTask.length)
-                addClickModal();
-        }
-        else
-        {
-            count++;
-            specificTaskContent = '<li class="specific-task" id="'+i+'">'+
-                  '<span class="category-icon" onclick="changeState("'+i+')">'+
-                  '<i class="fa fa-check-circle" aria-hidden="true"></i></span>'+
-                  '<span class="category-label">'+allTask[i].title+'</span>'+
-                  '<span class="category-modal-icon trigger" onclick="changeState("'+i+')">'+
-                  '<i class="fa fa-ellipsis-v">'+
-                  '</i><i class="fa fa-ellipsis-v"></i></span></li>';
-
-            taskListContent.innerHTML += specificTaskContent;
-
-            if(count == allTask.length)
-                addClickModal();
-       
-        }
-       }
-     }
-
- }
-
- function addTaskView()
+function all()
 {
-    var addTaskContent = '<div class="grid add-task">'+
-            '<button id="add-btn" onclick="addTask()">Add New Task</button>'
-         '</div>';
+   currentCategory = "all";
+
+    var allTask = JSON.parse(localStorage.getItem("allTask"));
+    var taskListContent = document.getElementById("taskList");
+    taskListContent.innerHTML = "";
+    var count = 0;
+
+    if (allTask != null) {
+        for (var i = 0; i < allTask.length; i++) {
+            var specificTaskContent;
+            if (allTask[i].status == 1) {
+                count++;
+                specificTaskContent = '<li class="specific-task" id="' + i + '">' +
+                    '<span class="category-icon" onclick="changeState(' + i + ')">' +
+                    '<i class="fa fa-circle-o" aria-hidden="true"></i></span>' +
+                    '<span class="category-label">' + allTask[i].title + '</span>' +
+                    '<span class="category-modal-icon trigger">' +
+                    '<i class="fa fa-ellipsis-v">' +
+                    '</i><i class="fa fa-ellipsis-v"></i></span></li>';
+
+                taskListContent.innerHTML += specificTaskContent;
+
+                if (count == allTask.length)
+                    addClickModal();
+            } else {
+                count++;
+                specificTaskContent = '<li class="specific-task" id="' + i + '">' +
+                    '<span class="category-icon" onclick="changeState(' + i + ')">' +
+                    '<i class="fa fa-check-circle" aria-hidden="true"></i></span>' +
+                    '<span class="category-label">' + allTask[i].title + '</span>' +
+                    '<span class="category-modal-icon trigger">' +
+                    '<i class="fa fa-ellipsis-v">' +
+                    '</i><i class="fa fa-ellipsis-v"></i></span></li>';
+
+                taskListContent.innerHTML += specificTaskContent;
+
+                if (count == allTask.length)
+                    addClickModal();
+            }
+        }
+    }
+}
+
+function showData() {
+   
+   //var currentClicked = document.getElementsByClassName("clicked")[0];
+   //currentClicked.classList.remove("clicked");
+   //console.log(currentClicked.classList);
+
+   if(currentCategory == "all")
+   {
+   // var currentCategoryClass = document.getElementById("all");
+   // currentCategoryClass.classList.add("clicked");
+    all();
+   }
+   if(currentCategory == "completed")
+   {
+   // var currentCategoryClass = document.getElementsByClassName("completed")[0];
+   // currentCategoryClass.classList.add("clicked");
+    completed();
+   }
+   if(currentCategory == "active")
+   {
+    // var currentCategoryClass = document.getElementsByClassName("active")[0];
+    // currentCategoryClass.classList.add("clicked");
+     active();
+   }
+
+}
+
+function addTaskView() {
+    var addTaskContent = '<div class="grid add-task">' +
+        '<button id="add-btn" onclick="addTask()">Add New Task</button>'
+    '</div>';
     container.innerHTML += addTaskContent;
-    taskCategoryView();     
+    taskCategoryView();
 }
 
-function taskCategoryView()
-{
-    var taskCategoryContent = '<div class="grid task-category">'+
-            '<nav>'+
-            '<span class="all" onclick="showData()" >'+
-            'All'+
-            '</span>'+
-            '<span class="completed" onclick="completed()" >'+
-            'Completed'+   
-            '</span>'+
-            '<span class="active" onclick="active()" >'+
-            'Active'+
-            '</span>'+
-            '</nav>'+
-         '</div>';
+function taskCategoryView() {
+    var taskCategoryContent = '<div class="grid task-category">' +
+        '<nav>' +
+        '<span class="all" id="all" onclick="showData()" >' +
+        'All' +
+        '</span>' +
+        '<span class="completed" id="completed" onclick="completed()" >' +
+        'Completed' +
+        '</span>' +
+        '<span class="active" id="active" onclick="active()" >' +
+        'Active' +
+        '</span>' +
+        '</nav>' +
+        '</div>';
     container.innerHTML += taskCategoryContent;
-    taskListView();      
+    taskListView();
 }
 
-function taskListView()
-{
-    var taskListContent = '<div class="grid task-list">'+
-            '<ul id="taskList">'+
-            '</ul>'+
-         '</div>'
+function taskListView() {
+    var taskListContent = '<div class="grid task-list">' +
+        '<ul id="taskList">' +
+        '</ul>' +
+        '</div>'
     container.innerHTML += taskListContent;
-    showData();     
+    showData();
 }
 
- function modalView()
- {
-     var modalContent = '<div class="modal">'+
-                  '<div class="modal-content">'+
-                  '<div class="edit" onclick="editTask()">'+
-                '<i class="fa fa-pencil" aria-hidden="true"></i><span class="edit-label">Edit</span>'+
-            '</div>'+
-            '<div class="interval">'+
-               'interval'+
-            '</div>'+
-            '<div class="delete" onclick="deleteTask()">'+
-               '<i class="fa fa-trash" aria-hidden="true"></i><span class="delete-label">Delete</span>'+
-            '</div>'+
-        '</div>'+
-    '</div>'
+function modalView() {
+    var modalContent = '<div class="modal">' +
+        '<div class="modal-content">' +
+        '<div class="edit" onclick="editTask()">' +
+        '<i class="fa fa-pencil" aria-hidden="true"></i><span class="edit-label">Edit</span>' +
+        '</div>' +
+        '<div class="interval">' +
+        'interval' +
+        '</div>' +
+        '<div class="delete" onclick="deleteTask()">' +
+        '<i class="fa fa-trash" aria-hidden="true"></i><span class="delete-label">Delete</span>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
 
     body.innerHTML += modalContent;
 
     addClickModal();
- } 
-  
-  function addClickModal()
-  {
-    var allTrigger = document.getElementsByClassName("trigger"); 
+}
 
-    for(var i = 0;i<allTrigger.length; i++)
-    {
-    allTrigger[i].addEventListener("click", updateListItem);
+function addClickModal() {
+    var allTrigger = document.getElementsByClassName("trigger");
+
+    for (var i = 0; i < allTrigger.length; i++) {
+        allTrigger[i].addEventListener("click", updateListItem);
     }
     window.addEventListener("click", windowOnClick);
-  }
+}
 
-  function updateListItem()
-  {
-    console.log(this.parentNode.getAttribute('id'));
+function updateListItem() {
     currentIndex = this.parentNode.getAttribute('id');
     toggleModal();
-  }
+}
 
-    function toggleModal() {
-        modal = document.querySelector(".modal");
-        modal.classList.toggle("show-modal");
-    }
+function toggleModal() {
+    modal = document.querySelector(".modal");
+    modal.classList.toggle("show-modal");
+}
 
-    function windowOnClick(event) {
-        if (event.target === modal) {
-            toggleModal();
-        }
-    }
-     
-     function deleteTask()
-     {
-        console.log(currentIndex);
-        var allTask = JSON.parse(localStorage.getItem("allTask"));
-        allTask.splice(currentIndex,1);
-        localStorage.setItem("allTask",JSON.stringify(allTask));
+function windowOnClick(event) {
+    if (event.target === modal) {
         toggleModal();
-        showData();
-     }
+    }
+}
 
-     function editTask()
-     {
-        console.log(currentIndex);
-        var allTask = JSON.parse(localStorage.getItem("allTask"));
-        var taskValue = allTask[currentIndex];
-        console.log(taskValue);
-        document.getElementsByTagName('input')[0].value = taskValue.title;
-        document.getElementById('add-btn').innerHTML = "Save";
-        isEdit = true;
-        toggleModal();
-     }
+function deleteTask() {
+    var allTask = JSON.parse(localStorage.getItem("allTask"));
+    allTask.splice(currentIndex, 1);
+    localStorage.setItem("allTask", JSON.stringify(allTask));
+    toggleModal();
+    showData();
+}
 
-     function changeState()
-     {
-        currentIndex = event.srcElement.parentNode.id;
-        console.log(currentIndex);
-        var allTask = JSON.parse(localStorage.getItem("allTask"));
-        if(allTask[currentIndex].status==0)
-          allTask[currentIndex].status=1;
-        else
-          allTask[currentIndex].status=0;
-        localStorage.setItem("allTask",JSON.stringify(allTask));
+function editTask() {
+    var allTask = JSON.parse(localStorage.getItem("allTask"));
+    var taskValue = allTask[currentIndex];
+    document.getElementsByTagName('input')[0].value = taskValue.title;
+    document.getElementById('add-btn').innerHTML = "Save";
+    isEdit = true;
+    toggleModal();
+}
 
-        showData();
-     }
+function changeState(index) {
+    currentIndex = index;
+    var allTask = JSON.parse(localStorage.getItem("allTask"));
+    if (allTask[currentIndex].status == 0)
+        allTask[currentIndex].status = 1;
+    else
+        allTask[currentIndex].status = 0;
+    localStorage.setItem("allTask", JSON.stringify(allTask));
+    
+    showData();
+}
 
-     function completed()
-     {
-       
-     var allTask = JSON.parse(localStorage.getItem("allTask"));
-     var taskListContent = document.getElementById("taskList");
-     taskListContent.innerHTML = "";
-     console.log(taskListContent);
 
-     if(allTask!=null)
-     {
-     for (var i = 0; i < allTask.length; i++) {
-        var specificTaskContent;
-        if(allTask[i].status == 1)
-        {
-            specificTaskContent = '<li class="specific-task">'+
-                  '<span class="category-icon"'+'id="'+i+'"onclick="changeState()">'+
-                  '<i class="fa fa-circle-o" aria-hidden="true"></i></span>'+
-                  '<span class="category-label">'+allTask[i].title+'</span>'+
-                  '<input type="hidden" value="'+i+'">'+
-                  '<span class="category-modal-icon trigger">'+
-                  '<i class="fa fa-ellipsis-v">'+
-                  '</i><i class="fa fa-ellipsis-v"></i></span></li>';
 
-            taskListContent.innerHTML += specificTaskContent;
+function completed() {
+
+    currentCategory = "completed";
+
+    var allTask = JSON.parse(localStorage.getItem("allTask"));
+    var taskListContent = document.getElementById("taskList");
+    taskListContent.innerHTML = "";
+
+    if (allTask != null) {
+        for (var i = 0; i < allTask.length; i++) {
+            var specificTaskContent;
+            if (allTask[i].status == 1) {
+                    specificTaskContent = '<li class="specific-task" id="' + i + '">' +
+                    '<span class="category-icon" onclick="changeState(' + i + ')">' +
+                    '<i class="fa fa-circle-o" aria-hidden="true"></i></span>' +
+                    '<span class="category-label">' + allTask[i].title + '</span>' +
+                    '<span class="category-modal-icon trigger">' +
+                    '<i class="fa fa-ellipsis-v">' +
+                    '</i><i class="fa fa-ellipsis-v"></i></span></li>';
+
+                taskListContent.innerHTML += specificTaskContent;
+            }
+            if (i == allTask.length - 1)
+                addClickModal();
         }
-        if(i==allTask.length-1)
-        addClickModal();
-       }
-     }
-     }
+    }
+}
 
-      function active()
-     {
-       
-     var allTask = JSON.parse(localStorage.getItem("allTask"));
-     var taskListContent = document.getElementById("taskList");
-     taskListContent.innerHTML = "";
-     console.log(taskListContent);
+function active() {
+    
+    currentCategory = "active";
 
-     if(allTask!=null)
-     {
-     for (var i = 0; i < allTask.length; i++) {
-        var specificTaskContent;
-        if(allTask[i].status == 0)
-        {
-            specificTaskContent = '<li class="specific-task">'+
-                  '<span class="category-icon"'+'id="'+i+'"onclick="changeState()">'+
-                  '<i class="fa fa-check-circle" aria-hidden="true"></i></span>'+
-                  '<span class="category-label">'+allTask[i].title+'</span>'+
-                  '<input type="hidden" value="'+i+'">'+
-                  '<span class="category-modal-icon trigger">'+
-                  '<i class="fa fa-ellipsis-v">'+
-                  '</i><i class="fa fa-ellipsis-v"></i></span></li>';
+    var allTask = JSON.parse(localStorage.getItem("allTask"));
+    var taskListContent = document.getElementById("taskList");
+    taskListContent.innerHTML = "";
+    console.log(taskListContent);
 
-            taskListContent.innerHTML += specificTaskContent;
+    if (allTask != null) {
+        for (var i = 0; i < allTask.length; i++) {
+            var specificTaskContent;
+            if (allTask[i].status == 0) {
+                specificTaskContent = '<li class="specific-task" id="' + i + '">' +
+                    '<span class="category-icon" onclick="changeState(' + i + ')">' +
+                    '<i class="fa fa-check-circle" aria-hidden="true"></i></span>' +
+                    '<span class="category-label">' + allTask[i].title + '</span>' +
+                    '<span class="category-modal-icon trigger">' +
+                    '<i class="fa fa-ellipsis-v">' +
+                    '</i><i class="fa fa-ellipsis-v"></i></span></li>';
+
+                taskListContent.innerHTML += specificTaskContent;
+            }
+            if (i == allTask.length - 1)
+                addClickModal();
         }
-        if(i==allTask.length-1)
-        addClickModal();
-       }
-     }
-     }
+    }
+}

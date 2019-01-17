89450a3b93de8c3623b25674e882b704a95b6cbb
Rana <alimur.razi@selise.ch>
2019-01-17 10:40:43 +0600

    firebase hoy na

--- 5th_task/angular-app/src/app/table/table.component.ts
+++ 5th_task/angular-app/src/app/table/table.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { PersonDataService } from '../person-data.service';
 import { DataSource } from '@angular/cdk/collections';
+import { MatTableDataSource, MatSort, MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-table',
@@ -11,32 +12,30 @@ import { DataSource } from '@angular/cdk/collections';
 })
 export class TableComponent implements OnInit {
 //  displayedColumns = ['FirstName', 'LastName', 'Email', 'Geneder', 'DOB', 'Email', 'Phone']
-  displayedColumns = ['FirstName', 'LastName']
-  dataSource = new PersonDataSource(this.person);
+ 
+  displayedColumns = ['FirstName', 'LastName'];
+  dataSource: MatTableDataSource<any>;
 
-  // persons: Observable<any[]>;
-  constructor(db: AngularFirestore, private person: PersonDataService) {
+  constructor(private db: AngularFirestore) {
     //  this.persons = db.collection('persons').valueChanges();
     //  console.log(db.collection('persons').valueChanges());
     //  console.log(this.persons);
   }
 
-  ngOnInit() {
+  ngOnInit(){
+    debugger;
+    this.db.collection<any>('persons').valueChanges().subscribe(data=>{
+      this.dataSource = new MatTableDataSource(data);
+      console.log(data);
+    })
   }
 
-}
-
-export class PersonDataSource extends DataSource<any>{
-  constructor(private person: PersonDataService) {
-    super();
-  }
-
-  connect() {
-    return this.person.getPerson();
-  }
-
-  disconnect() {
-
+  ngAfterViewInit(){
+    debugger;
+    this.db.collection<any>('persons').valueChanges().subscribe(data=>{
+      this.dataSource = new MatTableDataSource(data);
+      console.log(data);
+    })  
   }
 
 }

661354864553e132ac118e34ae2130683f3d1f8e
Alimur Razi Rana <alimurrazi570@gmail.com>
2019-05-21 10:39:28 +0600

    module add

---
+++ angular_tutorial/number-formatter/src/app/modules/number-formatter/number-formatter.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NumberFormatterComponent } from './number-formatter.component';
+
+@NgModule({
+  declarations: [NumberFormatterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NumberFormatterModule { }

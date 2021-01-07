bed54b06de1b46782e98895a73ee01697a1bf8a1
Alimurrazi <alimurrazi570@gmail.com>
2021-01-07 09:52:03 +0600

    old projects remove

--- 4th_task/out/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getDataService_1 = require("./getDataService");
-var data = new getDataService_1.getDataService();
-data.getUserData("alimurrazi");
--- angular_tutorial/number-formatter/public_api.ts
+++
@@ -1,1 +0,0 @@
-export * from './src/app/modules/number-formatter/number-formatter.module'

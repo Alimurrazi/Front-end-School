ba71e8e175998565703af4a7aa91a8abb32df3b0
Alimurrazi <alimurrazi570@gmail.com>
2021-01-12 09:44:46 +0600

    ngrx store worked

--- angular-redux/src/app/app.component.ts
+++ angular-redux/src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from '../store';
+import { CounterActions } from './app.actions';
+import { OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'angular-redux';
   count: number;
+  subscription;
 
-  increment() {}
-  decrement() {}
+  constructor(private ngRedux: NgRedux<IAppState>,
+              private actions: CounterActions) {
+    this.subscription = ngRedux.select<number>('count')
+          .subscribe(newCount => this.count = newCount);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  increment() {
+    this.ngRedux.dispatch(this.actions.increment());
+  }
+
+  decrement() {
+    this.ngRedux.dispatch(this.actions.decrement());
+  }
 }
--- angular-redux/src/app/app.module.ts
+++ angular-redux/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgReduxModule, NgRedux } from '@angular-redux/store';
 import { AppComponent } from './app.component';
+import { rootReducer, IAppState, INITIAL_STATE } from '../store';
+import { CounterActions } from './app.actions';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,14 @@ import { AppComponent } from './app.component';
     BrowserModule,
     NgReduxModule
   ],
-  providers: [],
+  providers: [CounterActions],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+    ngRedux.configureStore(
+      rootReducer,
+      INITIAL_STATE
+    );
+  }
+}
